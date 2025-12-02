efc01f645c707e23cace412cb0c78ad7b3081ea7
Noam-Mazan2025-katza <noam00707@gmail.com>
2025-12-02 16:54:39 +0200

    לוח הכפל

---
+++ MorielWebSite/AspNet.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage1.master" AutoEventWireup="true" CodeFile="AspNet.aspx.cs" Inherits="AspNet" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <center> 
+        <h2>multiply bord</h2>
+</center>
+    <center>
+     <%=strkefel%>
+    </center>
+
+
+</asp:Content>
+
---
+++ MorielWebSite/AspNet.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AspNet : System.Web.UI.Page
+{
+    public string strkefel;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        strkefel = "<table border = '1' width =70%";
+
+        for(int i = 1; i <= 10; i++)
+        {
+            strkefel += "<tr style=\"backgroynd-color: yellow\">";
+            for(int j = 1; j <= 10; j++)
+            {
+                strkefel += "<td>";
+                strkefel += i * j;
+                strkefel += "</td>";
+            }
+            strkefel += "</tr>";
+        }
+
+        strkefel += "</table>";
+    }
+}
