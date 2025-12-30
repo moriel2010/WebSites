c487faa2b992781aebe7ddec528fbffae6d07706
moriel <mondihash@gmail.com>
2025-12-30 16:04:50 +0200

    לוח הכפל

---
+++ MorielWebSite/Register1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register1 : System.Web.UI.Page
+{
+    public string str;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        str = "<table border = '1'>";
+        for (int i = 1; i < 11; i++)
+        {
+            str = str + "<tr>";
+            for (int j = 1; j < 11; j++)
+            {
+                str = str + "<td>";
+                str = str + i * j;
+                str = str + "</td>";
+            }
+            str = str + "</tr>";
+        }
+        str = str + "</table>";
+    }
+}
