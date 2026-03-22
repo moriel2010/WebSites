0e92a027e8766944bdc13a530245a887f46b4922
moriel <mondihash@gmail.com>
2026-03-22 15:56:33 +0200

    עשיתי משימה פסח ותחילת אתר אישי

---
+++ MorielWebSite/AddPage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage1.master" AutoEventWireup="true" CodeFile="enrollmentPage.aspx.cs" Inherits="enrollmentPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h2>תוספת כדורגל</h2>
+    <form name="formPage" method="post" runat="server">
+        גיל: <input type="age" name="age" id="age" placeholder="example">
+        <br />
+         גולים: <input type="goal" name="goals" id="goals" placeholder="example">
+        <br />
+         ליגה: <input type="text" name="league" id="league" placeholder="example">
+        <br />
+         כדורי זהב: <input type="ball" name="Goldballs" id="Goldballs" placeholder="example">
+        <br />
+         קבוצה נוכחי: <input type="team" name="team" id="team" placeholder="example">
+        <br />
+       
+        <input id="Submit1" type="submit" value="שלח" />
+    </form>
+
+</asp:Content>
+
---
+++ MorielWebSite/AddPage.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.UI;
+
+public partial class enrollmentPage : System.Web.UI.Page
+{
+    public string strResult = "";
+    public string s = "";
+
+    public string age;
+    public string goals;
+    public string league;
+    public string Goldballs;
+    public string team;
+   
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            age = Request.Form["age"];
+            goals = Request.Form["goal"];
+            league = Request.Form["text"];
+            Goldballs = Request.Form["ball"];
+            team = Request.Form["team"];
+           
+
+            string sqlSelect =
+                "INSERT INTO MorielFootball (age, goals, league, Goldballs, team) VALUES (" + 
+                 age + "'," +
+                 goals + "'," +
+                 "N'" + league + "," +
+                Goldballs + "," +
+                "N'" + team +
+                ")";
+
+            bool userExists = MyAdoHelper.IsExist(sqlSelect);
+
+           
+    }
+}
