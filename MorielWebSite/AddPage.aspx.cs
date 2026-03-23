9402dfd1caa75f6525a696c13f93afeefa3a5916
moriel <mondihash@gmail.com>
2026-03-23 16:15:10 +0200

    סידרתי

--- MorielWebSite/AddPage.aspx.cs
+++ MorielWebSite/AddPage.aspx.cs
@@ -3,38 +3,33 @@ using System.Web.UI;
 
 public partial class enrollmentPage : System.Web.UI.Page
 {
-    public string strResult = "";
-    public string s = "";
-
     public string age;
     public string goals;
     public string league;
     public string Goldballs;
     public string team;
-   
 
     protected void Page_Load(object sender, EventArgs e)
     {
         if (IsPostBack)
         {
             age = Request.Form["age"];
-            goals = Request.Form["goal"];
-            league = Request.Form["text"];
-            Goldballs = Request.Form["ball"];
+            goals = Request.Form["goals"];
+            league = Request.Form["league"];
+            Goldballs = Request.Form["Goldballs"];
             team = Request.Form["team"];
-           
 
-            string sqlSelect =
-                "INSERT INTO MorielFootball (age, goals, league, Goldballs, team) VALUES (" + 
-                 age + "'," +
-                 goals + "'," +
-                 "N'" + league + "," +
+            string sqlInsert =
+                "INSERT INTO MorielFootball " +
+                "(age, goals, league, Goldballs, team) VALUES (" +
+                age + "," +
+                goals + "," +
+                "N'" + league + "'," +
                 Goldballs + "," +
-                "N'" + team +
+                "N'" + team + "'" +
                 ")";
 
-            bool userExists = MyAdoHelper.IsExist(sqlSelect);
-
-           
+            MyAdoHelper.DoQuery(sqlInsert);
+        }
     }
 }
