e6267db76681dcfa525f8007b2d3ee98d43b87a8
user <user@DESKTOP-LHNE1P3>
2026-06-01 18:38:53 +0300

    aa

---
+++ MorielWebSite/shlifaPage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage1.master" AutoEventWireup="true" CodeFile="shlifaPage.aspx.cs" Inherits="shlifaPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h2>שליפה</h2>
+<form name="formPage" method="post" runat="server">
+    עיר:
+    <input type="text" name="city" id="city" placeholder="שם העיר" />
+    <br />
+    השיר הכי טוב:
+    <input type="text" name="best_song" id="best_song" placeholder="שם השיר הכי טוב" />
+    <br />
+    <input id="Submit1" type="submit" value="שלוף" />
+</form>
+<br />
+<%= st %>
+</asp:Content>
+
+
+    
---
+++ MorielWebSite/shlifaPage.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class shlifaPage : System.Web.UI.Page
+{
+    public string st = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            string city = Request.Form["city"];
+            string best_song = Request.Form["best_song"];
+
+            string sql = "SELECT * FROM morielAdd WHERE 1=1";
+
+            if (!string.IsNullOrEmpty(city))
+            {
+                sql += " AND city LIKE N'%" + city + "%'";
+            }
+
+            if (!string.IsNullOrEmpty(best_song))
+            {
+                sql += " AND best_song LIKE N'%" + best_song + "%'";
+            }
+
+            DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
+
+            if (dt.Rows.Count == 0)
+            {
+                st = "אין נתונים";
+            }
+            else
+            {
+                st += "<table border='1' style='color: white;'>";
+                st += "<tr>";
+                st += "<td>גיל</td>";
+                st += "<td>עיר</td>";
+                st += "<td>עוקבים</td>";
+                st += "<td>פרסים</td>";
+                st += "<td>אלבומים</td>";
+                st += "<td>השיר הכי טוב</td>";
+                st += "</tr>";
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    st += "<tr>";
+
+                    for (int k = 0; k < dt.Columns.Count; k++)
+                    {
+                        st += "<td>" + dt.Rows[i][k] + "</td>";
+                    }
+
+                    st += "</tr>";
+                }
+
+                st += "</table>";
+            }
+        }
+    }
+}
